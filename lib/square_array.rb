4362e1756e8f8a7e48344c8be9262fee211badd4
Josh Gumerove <josh.gumerove@gmail.com>
2020-11-19 17:43:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   array = [1, 2, 3]
   new_array = []
   count = 0
-  while count < 4
+  while count < array.length do
     puts new_array.push(array [count] ** 2)
     count += 1
   end  # your code here
