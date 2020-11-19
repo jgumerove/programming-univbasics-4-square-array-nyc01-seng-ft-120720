9ab421dec065f2326cebe24a01c1beec88fa6275
Josh Gumerove <josh.gumerove@gmail.com>
2020-11-19 17:29:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,5 +8,13 @@ new_array = []
   count += 1
   end  # your code here
   new_array
+  array = [9, 10, 16, 25]
+  new_array = []
 
+    count = 0
+    while count < array.length do
+    puts new_array.push(array [count] ** 2)
+    count += 1
+    end  # your code here
+    new_array
 end
